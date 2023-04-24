b5c998b82c5ec121f242bf6384bf9900f4628023
Abhijeet Deori <69836266+abhijeetdeori@users.noreply.github.com>
2023-04-23 20:35:47 -0400

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,12 @@
+module tetris.master {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.swt;
+    requires javafx.web;
+
+    opens application;
+}
